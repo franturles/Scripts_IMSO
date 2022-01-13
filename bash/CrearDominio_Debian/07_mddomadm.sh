13e38b3ec51ba782bf4c8315b3ea2357dd9149df
franturles <72397364+franturles@users.noreply.github.com>
2022-01-13 10:44:23 +0100

    Delete bash/CrearDominio_Debian directory

--- bash/CrearDominio_Debian/07_mddomadm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#INSTALAMOS LAS LBL-TOOLS NECESARIAS PARA CREAR LAS UNIDADES ORGRANITIVAS
-apt-get update && apt-get install ldb-tools -y
-
-ldbmodify -H /var/lib/samba/private/sam.ldb 07_moddomadm.ldif
-
